bcf75b6d2491e62cf2dbd1befba0f7ff527d5e28
imaizumikeisuke0508 <keisuke1158himajin@gmail.com>
2023-06-21 01:27:32 +0000

    fin step13

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def new
+    @book_image = Book.new
+  end
+
+  def create
+    @book_image = Book.new(book_image_params)
+    @book_image.user_id = current_user.id
+    @book_image.save
+    redirect_to books_path
+
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def book_image_params
+    params.require(:book).permit(:book_name, :image, :caption)
+  end
+
+end
