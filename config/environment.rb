1212f7b3f58d8fa4ef46696bd1ab4e97303eb2c4
imaizumikeisuke0508 <keisuke1158himajin@gmail.com>
2023-06-23 00:50:22 +0000

    fin step17

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the Rails application.
 require_relative "application"
 
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
