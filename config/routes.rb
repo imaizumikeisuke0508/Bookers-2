2fd2ef7f38de5b37b6b0f5d424ef92069938d04a
EC2 Default User <ec2-user@ip-172-31-35-173.ap-northeast-1.compute.internal>
2023-07-11 00:44:09 +0000

    step30 edit

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   
-  resources :books, only:[:new, :create, :index, :show, :edit, :destroy]
+  resources :books, only:[:new, :create, :index, :show, :edit, :destroy, :update]
   resources :users, only:[:edit, :show, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
